ac27e71911794e98cb56c6fcba9dea19f4183694
Darko Hojnik <darko@hojnik.de>
2017-06-01 17:53:26 +0200

    rabbitmq fixures

--- site/profiles/manifests/rabbitmq.pp
+++ site/profiles/manifests/rabbitmq.pp
@@ -10,7 +10,6 @@ class profiles::rabbitmq (
     password => $rabbit_password,
   }
 
-
   rabbitmq_user_permissions { "${rabbit_user}@/":
     configure_permission => '.*',
     read_permission      => '.*',
